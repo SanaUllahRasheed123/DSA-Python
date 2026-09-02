f6997c8cbce7fc2ea816784b233edacc62e6990a
sanaullah123 <starsreachersacademy@gmail.com>
2026-09-02 10:16:54 +0500

    updated

--- dsa.py
+++ dsa.py
@@ -1,8 +1,48 @@
 # print("Hello world)
 
 
+# def print_items(n):
+#     for i in range(n):
+#         print(i)
+        
+# print_items(11)
+
+# Big O drop constraints
+
+# def print_items(n):
+#     for i in range(n):
+#         print(i)
+        
+#     for j in range(n):
+#         print(j)
+        
+        
+# print_items(10 )
+
+# Big O O(n^2)
+# def print_items(n):
+#     for i in range(n):
+#         for j in range(n):
+#             for k in range(n):
+#                 print(i,j,k)
+            
+# print_items(10)
+
+# Big O Drop Non-Dominants
+
+
+
 def print_items(n):
     for i in range(n):
-        print(i)
+        for j in range(n):
+            print(i,j)
+            
+    for k in range(n):
+        print(k)
         
-print_items(11)
+print_items(10)
+
+# Big O O(1)
+
+# Big O (1)
+        
