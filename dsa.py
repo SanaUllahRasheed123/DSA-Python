6cadec782a331c79c8f541cb8603ced1ddff940b
sanaullah123 <starsreachersacademy@gmail.com>
2026-09-07 12:06:41 +0500

    updated1

--- dsa.py
+++ dsa.py
@@ -16,13 +16,6 @@
 #         print(j)
 # print_items(10)
 
-def print_items(n):
-    for i in range(n):
-        print(i)
-    for j in range(n):
-        print(j)
-print_items(19)
-
 # Big O O(n^2)
 # def print_items(n):
 #     for i in range(n):
@@ -32,19 +25,26 @@ print_items(19)
             
 # print_items(10)
 
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                print(i,j,k)
+print_items(10)
+        
+    
+
 # Big O Drop Non-Dominants
 
-
-
-# def print_items(n):
-#     for i in range(n):
-#         for j in range(n):
-#             print(i,j)
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
             
-#     for k in range(n):
-#         print(k)
+    for k in range(n):
+        print(k)
         
-# print_items(10)
+print_items(10)
 
 # Big O O(1)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,9 @@
 def print_items(n):
     for i in range(n):
-        print(i)        
-print_items(11)
+        for j in range(n):
+            print(i,j)
+            
+    for k in range(n):
+        print(k)
+        
+print_items(10)
