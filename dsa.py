8a8e5924a6a30f792fbba7fc3f10cefc6979554d
sanaullah123 <starsreachersacademy@gmail.com>
2026-09-08 23:22:46 +0500

    updated

--- dsa.py
+++ dsa.py
@@ -54,11 +54,27 @@
 
 
 # Classes
-class Cookie:
-    def __init__(self,color):
-        self.color=color
-cookie_one=Cookie(color="Green")
-print(cookie_one)
+# class Cookie:
+#     def __init__(self,color):
+#         self.color=color
+        
+#     def get_color(self):
+#         return self.color
+        
+#     def set_color(self,color):
+#         self.color=color
+    
+        
+# cookie_one=Cookie("green")
+# cookie_two = Cookie('blue')
+
+# print("Cookie one is ",cookie_one.get_color())
+# print("Cookie two is",cookie_two.get_color())
+
+# cookie_one.set_color("yellow")
+
+# print("\nCookie one is now",cookie_one.get_color())
+# print("Cookie two is still",cookie_two.get_color())
 
 #  
 
@@ -82,6 +98,29 @@ print(cookie_one)
 
 # print("\nnum1 points to:",id(num1))
 # print("num2 points to:", id(num2)) 
+
+dict1={
+    'value':11
+}
+
+dict2=dict1
+
+print("Before value is updated:")
+print("dict1",dict1)
+print("dict2",dict2)
+
+print("\ndict1 points to:",id(dict1))
+print("dict2 points to :",id(dict2))
+
+
+dict2['value']=23
+
+print("\ndict1 points to:",dict1)
+print("dict2 points to:",dict2)
+
+print("\ndict1 points to id:",id(dict1))
+print("dict2 points to id:",id(dict2))
+
 
     # dict1 ={
     #     'value':10
