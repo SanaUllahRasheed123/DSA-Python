1fffcfa5352dbd1f36d90e7a0ead8b2acb498862
sanaullah123 <starsreachersacademy@gmail.com>
2026-09-10 10:51:36 +0500

    updated

--- dsa.py
+++ dsa.py
@@ -125,23 +125,76 @@
 
 # Hello world
 
-print("Hello")
+# print("Hello")
 
-head ={
-     "value":11,
-        "next":{
-              "value":3,
-             "next":{
-            "value":23,
-            "next":{
-                "value":7,
-                "next":None
-                    }
-              }   
-            }
-        }
-print(head['next']['next']['next']['value'])
+# head ={
+#      "value":11,
+#         "next":{
+#               "value":3,
+#              "next":{
+#             "value":23,
+#             "next":{
+#                 "value":7,
+#                 "next":None
+#                     }
+#               }   
+#             }
+#         }
+# print(head['next']['next']['next']['value'])
 
 # This will only run with a Linked List
 
-print(head["next"]["next"]["next"]["value"])
+# print(head["next"]["next"]["next"]["value"])
+
+# LL Constructor
+
+# class Node:
+#     def __init__(self,value):
+#         self.value=value
+#         self.next=None
+# class LinkedList:
+#     def __init__(self,value):
+#         new_node=Node(value)
+#         self.head = new_node
+#         self.tail = new_node
+#         self.length=1
+        
+# my_linked_list = LinkedList(4)
+
+# print(my_linked_list.head.value)
+
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+        
+    
+class LinkedList:
+    def __init__(self,value):
+        new_node=Node(value)
+        self.head=new_node
+        self.tail=new_node
+        self.length=1
+        
+    def print_list(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+            
+    def append(self,value):
+        new_node=Node(value)
+        if self.length==0:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length+=1
+        
+my_linked_list=LinkedList(1)
+
+my_linked_list.append(2)
+
+my_linked_list.print_list()
+            
