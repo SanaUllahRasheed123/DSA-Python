699610d9febcb2fb3a6e9bebcaf971a8250119b9
sanaullah123 <starsreachersacademy@gmail.com>
2026-09-11 11:45:29 +0500

    updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,2 @@
-def print_items(n):
-#     for i in range(n):
-#         for j in range(n):
-#             print(i,j)
-            
-#     for k in range(n):
-#         print(k)
-        
-# print_items(10)
+my_linked_list.append(2)
+# my_linked_list.append(6)
