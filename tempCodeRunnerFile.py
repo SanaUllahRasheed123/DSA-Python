80c95e0854c3c814641281c0e4a040ba59928196
sanaullah123 <starsreachersacademy@gmail.com>
2026-09-17 21:40:30 +0500

    updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,41 @@
-my_linked_list.append(2)
-# my_linked_list.append(6)
+class Node:
+#     def __init__(self,value):
+#         self.value=value
+#         self.next = None
+    
+# class LinkedList:
+#     def __init__(self,value):
+#         new_node=Node(value)
+#         self.head=new_node
+#         self.tail=new_node
+#         self.length=1
+    
+#     def print_list(self):
+#         temp = self.head
+#         while temp is not None:
+#             print(temp.value)
+#             temp=temp.next
+            
+#     def append(self,value):
+#         new_node=Node(value)
+#         if self.length==0:
+#             self.head=new_node
+#         else:
+#             self.tail.next=new_node
+#             self.tail=new_node
+            
+#         self.length+=1
+        
+
+        
+    
+        
+# my_linked_list=LinkedList(23)
+
+# my_linked_list.append(24)
+
+# print("Head",my_linked_list.head.value)
+# print("Tail",my_linked_list.tail.value)
+# print("Length",my_linked_list.length)
+
+# my_linked_list.print_list()
